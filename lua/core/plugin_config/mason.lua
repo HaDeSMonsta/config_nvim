6e3c0fe15cedccb93730d16e719f39db855bebbd
HaDeSMonsta <leon.poellinger+git@gmail.com>
2023-11-19 23:51:36 +0100

    Added additional mason config

--- lua/core/plugin_config/mason.lua
+++ lua/core/plugin_config/mason.lua
@@ -28,3 +28,26 @@ vim.api.nvim_create_autocmd("fileType", {
   end,
   group = nvim_metals_group,
 })
+
+-- For some reson this has to be between the two Mason blocks, kickstart knows that better than me
+--
+-- Setup neovim lua configuration
+require("neodev").setup()
+
+-- Ensure the servers above are installed
+local mason_lspconfig = require "mason-lspconfig"
+
+mason_lspconfig.setup {
+  ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers {
+  function(server_name)
+    require("lspconfig")[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = servers[server_name],
+      filetypes = (servers[server_name] or {}).filetypes,
+    }
+  end,
+}
