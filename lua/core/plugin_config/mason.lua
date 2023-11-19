54ba4afcf1496a5d37c968ff235a0796a6506efe
HaDeSMonsta <leon.poellinger+git@gmail.com>
2023-11-19 23:41:37 +0100

    Added Mason config

--- lua/core/plugin_config/mason.lua
+++ lua/core/plugin_config/mason.lua
@@ -1,1 +1,30 @@
-print("// TODO write mason config")
+-- mason-lspconfig requires that these setup functions are called in this order
+-- before setting up the servers.
+require('mason').setup()
+require('mason-lspconfig').setup()
+
+local servers = {
+  clangd = {},
+  gopls = {},
+  pyright = {},
+  rust_analyzer = {},
+  tsserver = {},
+  html = { filetypes = { 'html', 'twig', 'hbs'} },
+
+  lua_ls = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+    },
+  },
+}
+
+-- Scala metals
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim_metals", { clear = true })
+vim.api.nvim_create_autocmd("fileType", {
+  pattern = { "scala", "sbt", "java" },
+  callback = function ()
+    require("metals").initialize_or_attach({})
+  end,
+  group = nvim_metals_group,
+})
