65e9ccd58d098fc297e2a44bc8044a3a79c8fa92
HaDeSMonsta <leon.poellinger+git@gmail.com>
2023-11-19 23:13:41 +0100

    Added diagnostic keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -36,3 +36,9 @@ vim.keymap.set({ "n" }, "<leader>", "<Nop>", { silent = true })
 -- if we have a count it will jump to the position with the appropriate relative line number 
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
