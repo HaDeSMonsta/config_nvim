69a7b037111be3c3ff1b9eb930369f8e5f8d48ce
HaDeSMonsta <leon.poellinger+git@gmail.com>
2023-11-22 17:26:57 +0100

    Changed indentation to 4 spaces from 2

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -11,9 +11,9 @@ vim.opt["guicursor"] = ""
 vim.opt.scrolloff = 8
 
 -- Define indentation
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- Make nvim automatically indent when appropriate
