12c42f844358882cfb35e95c817ac41d8198f324
HaDeSMonsta <leon.poellinger+git@gmail.com>
2024-02-13 18:20:05 +0100

    Updated Plugins, reconfigured keybinds and description

--- lua/core/plugin_config/harpoon.lua
+++ lua/core/plugin_config/harpoon.lua
@@ -1,8 +1,10 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Mark a file for Harpoon" })
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle quick menu" })
+vim.keymap.set("n", "<leader>ha", mark.add_file, { desc = "Add file" })
+vim.keymap.set("n", "<leader>hr", mark.rm_file, { desc = "Remove file" })
+vim.keymap.set("n", "<leader>hc", mark.clear_all, { desc = "Clear all" })
+vim.keymap.set("n", "<leader>hm", ui.toggle_quick_menu, { desc = "Toggle quick menu" })
 
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end, { desc = "Jump to harpoon marked file 1"})
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end, { desc = "Jump to harpoon marked file 2"})
